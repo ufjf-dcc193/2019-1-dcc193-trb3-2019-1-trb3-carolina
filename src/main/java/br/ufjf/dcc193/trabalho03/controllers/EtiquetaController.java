f243088ff27816b76ddafc69fac59e18dff3ffb8
Carnijh <carnijh@hotmail.com>
2019-07-02 11:43:33 -0300

    Controllers criados

---
+++ src/main/java/br/ufjf/dcc193/trabalho03/controllers/EtiquetaController.java
@@ -0,0 +1,12 @@
+package br.ufjf.dcc193.trabalho03.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * EtiquetaController
+ */
+@Controller
+public class EtiquetaController {
+
+    
+}
---
+++ src/main/java/br/ufjf/dcc193/trabalho03/controllers/ItemController.java
@@ -0,0 +1,12 @@
+package br.ufjf.dcc193.trabalho03.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * ItemController
+ */
+@Controller
+public class ItemController {
+
+    
+}
